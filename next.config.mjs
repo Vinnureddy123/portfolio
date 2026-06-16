bae65fec2fe5f675cd6e162207ba431ac07727f7
Vinnureddy123 <vinay.abintech@gmail.com>
2026-06-16 14:08:32 +0530

    git changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  basePath: '/portfolio',
+  assetPrefix: '/portfolio/',
   reactCompiler: true,
   images: {
+    unoptimized: true,
     qualities: [75, 80, 95, 100],
   },
 };
